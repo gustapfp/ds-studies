01b3f0875c8527c132c71e4e88ee80c81c5c2433
gustapfp <gustavopfp.trab@gmail.com>
2026-01-11 14:25:06 -0300

    feat: add mergeTwoLists function to LinkedListOperations for merging two sorted linked lists

--- algorithms/linked-list/invert_linked_list.py
+++ algorithms/linked-list/invert_linked_list.py
@@ -23,3 +23,17 @@ class LinkedListOperations:
             head = head.next()
 
         return head
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = dummy = ListNode()
+
+        while list1 and list2:
+            if list2.val > list1.val:
+                head.next = list1
+                list1, head = list1.next, list1
+            else:
+                head.next = list2
+                list2, head = list2.next, list2
+        if list1 or list2:
+            head.next = list1 if list1 else list2
+        return dummy.next
